3b781c6baa7dac569f8936b2a98e16ecd1edd33f
GoDandrea <dandrea.alves@gmail.com>
2022-07-11 16:45:43 -0300

    :recycle: Rewrote main to use new features

--- main.cc
+++ main.cc
@@ -1,9 +1,8 @@
-//#define STB_IMAGE_WRITE_IMPLEMENTATION
-//#include "stb_image_write.h"
+#include "rtweekend.h"
 
 #include "color.h"
-#include "ray.h"
-#include "vec3.h"
+#include "body_list.h"
+#include "sphere.h"
 
 #include <iostream>
 
@@ -12,32 +11,15 @@
 #define RED color(1,0,0);
 
 
-double hit_sphere(const point3 &center, double radius, const ray &r) {
-    vec3 oc = r.origin() - center;
-    auto a = r.direction().length_squared();
-    auto half_b = dot(oc, r.direction());
-    auto c = oc.length_squared() - radius*radius;
-    auto discriminant = half_b*half_b - a*c;
-
-    if (discriminant < 0) {
-        return -1.0;
-    } else {
-        return (-half_b - sqrt(discriminant)) / a;
+color ray_color(const ray &r, const body &world) {
+    
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + WHITE);
     }
-}
 
-
-color ray_color(const ray &r) {
-    
-    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
-    
-    if (t > 0.0) {
-        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
-        return 0.5 * color(N.x()+1, N.y()+1, N.z()+1);
-    }
-    
     vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5 * (unit_direction.y() + 1.0);
+    auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t)*WHITE + t*SKY_BLUE;
 }
 
@@ -52,6 +34,12 @@ int main() {
 
     unsigned char image_data[image_width * image_height * image_channels];
     int index = 0;
+
+
+    // World
+    body_list world;
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
 
     // Camera
@@ -76,7 +64,7 @@ int main() {
             auto v = double(j) / (image_height-1);
             ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
 
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             
             write_color(image_data, pixel_color, index);
             index += 3;
