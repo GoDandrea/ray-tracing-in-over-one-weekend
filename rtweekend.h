72ccb0da2dd4422ee257d5da277dce89f78985fd
GoDandrea <dandrea.alves@gmail.com>
2022-07-11 16:52:06 -0300

    :sparkles: Add new random_double utilities to rtweekend.b

--- rtweekend.h
+++ rtweekend.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <cstdlib>
 
 
 // Usings
@@ -11,17 +12,29 @@ using std::shared_ptr;
 using std::make_shared;
 using std::sqrt;
 
+
 // Constants
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
+
 
 // Utility functions
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
+inline double random_double() {
+    return rand() / (RAND_MAX + 1.0); // Returns a random real in [0,1)
+}
+
+inline double random_double(double min, double max) {
+    return min + (max-min) * random_double(); // Returns a random rean in [min,max)
+}
+
+
 // Common headers
 #include "ray.h"
 #include "vec3.h"
+
 
 #endif
