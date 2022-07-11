df24613bbaf7c5520a1d51e1016a21cb0a87625c
GoDandrea <dandrea.alves@gmail.com>
2022-07-11 09:31:44 -0300

    :sparkles: Render white-blue gradient (sky)

--- main.cc
+++ main.cc
@@ -2,34 +2,58 @@
 //#include "stb_image_write.h"
 
 #include "color.h"
+#include "ray.h"
 #include "vec3.h"
 
 #include <iostream>
 
+#define WHITE color(1.0,1.0,1.0)
+#define SKY_BLUE color(0.5,0.7,1.0)
+
+
+color ray_color(const ray &r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * WHITE + t * SKY_BLUE;
+}
+
 
 int main() {
 
-
     // Image
-
-    const int image_width = 256;
-    const int image_height = 256;
+    const auto aspect_ratio = 16.0/9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width/aspect_ratio);
     const int image_channels = 3;
 
     unsigned char image_data[image_width * image_height * image_channels];
     int index = 0;
 
 
-    // Render
+    // Camera
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
 
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal/2 - vertical/2 
+                                    - vec3(0, 0, focal_length);
+
+
+    // Render
     for (int j = image_height-1; j >= 0; --j) {
         
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
         
-            color pixel_color(double(i)/(image_width-1), 
-                              double(j)/(image_height-1), 
-                              0.25);
+            auto u = double(i) / (image_width-1);
+            auto v = double(j) / (image_height-1);
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+
+            color pixel_color = ray_color(r);
+            
             write_color(image_data, pixel_color, index);
             index += 3;
         }
