22e6b36d255b57c7db51a01768814abd47c32a36
GoDandrea <dandrea.alves@gmail.com>
2022-07-12 10:11:40 -0300

    :sparkles: Add multisampling to color.h

--- color.h
+++ color.h
@@ -9,10 +9,22 @@
 #include <iostream>
 
 
-void write_color(unsigned char *image_data, color pixel_color, int index) {
-    image_data[index+0] = static_cast<int>(255.999 * pixel_color.x());
-    image_data[index+1] = static_cast<int>(255.999 * pixel_color.y());
-    image_data[index+2] = static_cast<int>(255.999 * pixel_color.z());
+void write_color(unsigned char *image_data, color pixel_color, int index, int samples_per_pixel) {
+
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+    // Divide color by number of samples
+    auto scale = 1.0 / samples_per_pixel;
+    r *= scale;
+    g *= scale;
+    b *= scale;
+
+    // Write the translated [0,255] value of each component
+    image_data[index+0] = static_cast<int>(256 * clamp(r, 0.0, 0.999));
+    image_data[index+1] = static_cast<int>(256 * clamp(g, 0.0, 0.999));
+    image_data[index+2] = static_cast<int>(256 * clamp(b, 0.0, 0.999));
 }
 
 #endif
